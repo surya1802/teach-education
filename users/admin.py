c6a2f686183fe7beea39a4b726631f15f354818f
surya1802 <116265862+surya1802@users.noreply.github.com>
2025-09-17 06:55:43 +0530

    Delete users directory

--- users/admin.py
+++
@@ -1,9 +0,0 @@
-# Register your models here.
-
-from django.contrib import admin
-from .models import User
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'is_customer', 'is_staff', 'is_active')
-    search_fields = ('username', 'email')
